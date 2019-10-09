3a477b54f569cc49d797f31430a870947b218a43
Rodrigo Cerda Millan <rcerda@MOLEB.local>
2019-10-09 11:59:06 -0300

    Revert "Revert "version 2.0""
    This reverts commit 804bba771bd8991b9099d5f1988d5e2c86cb4697.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-script
+<script>
+        document.getElementById('link').onclick = function () {
+            console.log("entre al js");            
+            // console.log("datos form: "+ data);
+            //setTimeout(function () { location.href = "@Url.Action("GeneraDJ", "Procesos")" }, 3000);
+            setTimeout(function () {            
+                document.getElementById("form_dj").submit();            
+            }, 3000);            
+        };
+</script>
